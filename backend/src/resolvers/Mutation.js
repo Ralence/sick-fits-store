af5c0b62519fb265f7757426847172bf35a5c408
Ralence <ranko.ostojic@gmail.com>
2019-10-06 13:44:12 +0200

    feat: add createItem Mutation and items Quary

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const Mutations = {};
+const Mutations = {
+    async createItem(parents, args, ctx, info) {
+        // TODO Check if they are logged in
+
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info)
+
+        return item;
+    }
+    // createDog(parents, args, ctx, info) {
+    //     console.log(args)
+    // }
+};
 
 module.exports = Mutations;
