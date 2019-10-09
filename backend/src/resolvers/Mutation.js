907e94b8fbe5e8e0df3da3fc64b668e6e234cff2
Ralence <ranko.ostojic@gmail.com>
2019-10-09 20:11:53 +0200

    feat: add deleteItem mutation functionality

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,5 +1,5 @@
 const Mutations = {
-    async createItem(parents, args, ctx, info) {
+    async createItem(parent, args, ctx, info) {
         // TODO Check if they are logged in
 
         const item = await ctx.db.mutation.createItem({
@@ -9,10 +9,33 @@ const Mutations = {
         }, info)
 
         return item;
+    },
+
+    updateItem(parent, args, ctx, info) {
+        // First take a copy of updates
+        const updates = { ...args };
+        // Remove the id from the updates
+        delete updates.id;
+        // Run the update method
+        return ctx.db.mutation.updateItem({
+            data: updates,
+            where: {
+                id: args.id
+            }
+        },
+            info
+        )
+    },
+
+    async deleteItem(parent, args, ctx, info) {
+        const where = { id: args.id };
+        // 1 Find the item
+        const item = await ctx.db.query.item({ where }, `{ id title }`);
+        // 2 Check if the user owns the item or has a permission
+        // TODO
+        // 3 Delete the item
+        return ctx.db.mutation.deleteItem({ where }, info);
     }
-    // createDog(parents, args, ctx, info) {
-    //     console.log(args)
-    // }
 };
 
 module.exports = Mutations;
