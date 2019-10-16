712560e40626f223ceda21ecbf56ff4f0c96dc0c
Ralence <ranko.ostojic@gmail.com>
2019-10-16 16:06:48 +0200

    feat: add signin to schema and Mutations.js

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -63,6 +63,27 @@ const Mutations = {
         });
         // finally return the user to the browser
         return user;
+    },
+    async signin(parent, { email, password }, ctx, info) {
+        // 1. check if there is a user with that email
+        const user = await ctx.db.query.user({ where: { email } });
+        if (!user) {
+            throw new Error(`No user found for the email: ${email}`)
+        }
+        // 2. check if the password is correct
+        const valid = bcrypt.compare(password, user.password);
+        if (!valid) {
+            throw new Error(`Invalid Password!`);
+        }
+        // 3. generate JWT token
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        // 4. set the cookie with the token
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365,
+        });
+        // 5. return the user
+        return user;
     }
 };
 
