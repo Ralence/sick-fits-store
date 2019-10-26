51b4f519c8f7d4692441ccfc9cd85d63b69cb6cb
Ralence <ranko.ostojic@gmail.com>
2019-10-26 21:42:01 +0200

    feat: add CartItem data type and addToCart  mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -209,6 +209,37 @@ const Mutations = {
       info
     );
   },
+  async addToCart(parent, args, ctx, info) {
+    // 1 check if they are signed in
+    const { userId } = ctx.request;
+    if (!userId) {
+      throw new Error(`You must be signed in to do that!`);
+    }
+    // 2 query the user's current cart
+    const [existingCartItem] = await ctx.db.query.cartItems({
+      user: { id: userId },
+      item: { id: args.id },
+    });
+    // 3 check if the item is already in the cart
+    // and increment by one if it is
+    if (existingCartItem) {
+      return ctx.db.mutation.updateCartItem({
+        where: { id: existingCartItem.id },
+        data: { quantity: existingCartItem.quantity + 1 },
+      });
+    }
+    // 4 if it is not create a new CartItem for that user
+    return ctx.db.mutation.createCartItem({
+      data: {
+        user: {
+          connect: { id: userId },
+        },
+        item: {
+          connect: { id: args.id },
+        },
+      },
+    });
+  },
 };
 
 module.exports = Mutations;
